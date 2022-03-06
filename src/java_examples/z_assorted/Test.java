b421daddde6a1323135bddf03e0a4c76a8414d04
Gaurav Khanna <iamgauravkhanna@gmail.com>
2022-03-06 09:42:09 +0530

    changes

--- src/java_examples/z_assorted/Test.java
+++
@@ -1,9 +0,0 @@
-package java_examples.z_assorted;
-
-import java.util.HashMap;
-
-public class Test {
-    public static void main(String[] args){
-        HashMap<Character,Integer> hashMap = new HashMap<Character, Integer>();
-    }
-}
