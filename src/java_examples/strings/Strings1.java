2f586c1aa278c3043afdb29bc998b400f2b99bf0
Gaurav Khanna <iamgauravkhanna@gmail.com>
2022-04-21 09:21:17 +0530

    changes-13098

--- src/java_examples/strings/Strings1.java
+++ src/java_examples/strings/Strings1.java
@@ -2,12 +2,9 @@ package java_examples.strings;
 
 public class Strings1 {
 
-	public static void main(String[] args) {
-
-		char[] webSiteName = { 'G', 'A', 'U', 'R', 'A', 'V' };
-		String webSite = new String(webSiteName);
-		System.out.println(webSite);
-
-	}
-
+    public static void main(String[] args) {
+        char[] webSiteName = {'G', 'A', 'U', 'R', 'A', 'V'};
+        String webSite = new String(webSiteName);
+        System.out.println(webSite);
+    }
 }
